33062b2e53169f0a30ce06334c816055fefea17f
ice <meltedise@gmail.com>
2012-02-13 00:29:37 +0900

    added rspec.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in wareki.gemspec
 gemspec
+
+group :test do
+  gem 'rspec', '~> 2.0.0.beta.22'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+$:.unshift(File.dirname(__FILE__))
+
+require 'rubygems'
+require 'bundler'
+Bundler.setup
+
+require 'wareki'
