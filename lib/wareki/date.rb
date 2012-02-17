3fa85b29753497fd3aedc3a8fc86f2c0da9a97f4
ice <meltedise@gmail.com>
2012-02-18 03:45:09 +0900

    split huge method.

--- lib/wareki/date.rb
+++ lib/wareki/date.rb
@@ -12,19 +12,25 @@ module Wareki
           bom = Meiji.era_table[year][month]
           return ::Date.new(bom[:g_y], bom[:g_m], bom[:g_d]) + (mday - 1)
         end
+        return ::Date.parse(str)
       when /^([a-z]+)(\d+)\.(\d+)\.(\d+)/i
         era, year, month, mday = $1.downcase, $2.to_i, $3.to_i, $4.to_i
-        bom = nil
-        case era
-        when 'k', 'keio'
-          require 'wareki/date/keio'
-          bom = Keio.era_table[year][month]
-        else
-          raise 'Unknown era'
-        end
-        return ::Date.new(bom[:g_y], bom[:g_m], bom[:g_d]) + (mday - 1)
+        return _parse(era, year, month, mday)
       end
-      ::Date.parse(str)
     end # parse
+
+    private
+
+    def self._parse(era, year, month, mday)
+      bom = nil
+      case era
+      when 'k', 'keio'
+        require 'wareki/date/keio'
+        bom = Keio.era_table[year][month]
+      else
+        raise 'Unknown era'
+      end
+      ::Date.new(bom[:g_y], bom[:g_m], bom[:g_d]) + (mday - 1)
+    end
   end # Date
 end # Wareki
