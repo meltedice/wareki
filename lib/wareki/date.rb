af0da36e3cbf0b6f04b1169fd3506900c6a49ce1
ice <meltedise@gmail.com>
2012-02-24 02:54:54 +0900

    added kanji era name support.

--- lib/wareki/date.rb
+++ lib/wareki/date.rb
@@ -2,21 +2,41 @@
 
 module Wareki
   module Date
+    WAREKI_SHORT_ERA_NAME_TABLE = {'明' => 'm', '大' => 't', '昭' => 's', '平' => 'h'}
+    WAREKI_LONG_ERA_NAME_TABLE = {
+      '明治' => 'm',
+      '大正' => 't',
+      '昭和' => 's',
+      '平成' => 'h',
+    }
+
     def self.parse(str)
       # puts "str ==> #{str}"
-      case str
-      when /^([mtsh])(\d+)\.(\d+)\.(\d+)/i
+      if /^([mtsh])(\d+)\.(\d+)\.(\d+)/i =~ str
         era, year, month, mday = $1.downcase, $2.to_i, $3.to_i, $4.to_i
         if era == 'm' && year < 6
           return _parse(era, year, month, mday)
         else
           return ::Date.parse(str)
         end
-      when /^([^0-9]+)(\d+)\.(\d+)\.(\d+)/i
+      end
+      str = parse_kanji(str) if str[0] =~ /^[^0-9A-z]/
+      if /^([a-z]+)(\d+)\.(\d+)\.(\d+)/i =~ str
         era, year, month, mday = $1.downcase, $2.to_i, $3.to_i, $4.to_i
         return _parse(era, year, month, mday)
       end
+      nil
     end # parse
+
+    def self.parse_kanji(kanji_str)
+      str = kanji_str.dup
+      if kanji_str[1] =~ /[0-9]/
+        str[0] = WAREKI_SHORT_ERA_NAME_TABLE[str[0]]
+      else
+        str[0,2] = WAREKI_LONG_ERA_NAME_TABLE[str[0,2]]
+      end
+      str
+    end
 
     protected
 
